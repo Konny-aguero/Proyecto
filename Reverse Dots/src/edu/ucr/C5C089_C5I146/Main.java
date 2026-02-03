a51c7492490a898f88a6d6dff8c10d0195350317
Konny <konnyaguero@gmail.com>
2026-02-03 15:04:58 -0600

    Add files via upload

---
+++ Reverse Dots/src/edu/ucr/C5C089_C5I146/Main.java
@@ -0,0 +1,7 @@
+package edu.ucr.C5C089_C5I146;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Reverse Dots/src/edu/ucr/C5C089_C5I146/reversedots/model/Piece.java
@@ -0,0 +1,5 @@
+package edu.ucr.C5C089_C5I146.reversedots.model;
+
+public enum Piece {
+    BLACK, WHITE, EMPTY
+}
